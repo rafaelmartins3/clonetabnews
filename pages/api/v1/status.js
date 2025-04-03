ce9cb64f6525d55c2c162bf752242f8da62c3a5c
Rafael Martins <31152310+rafaelmartins3@users.noreply.github.com>
2025-04-03 21:12:55 +0000

    finish /status endpoint issue

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,9 +1,38 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  response.status(200).json("Alunos do curso.dev são acima da média");
+  const updated_at = new Date().toISOString();
+
+  const database_version_result = await database.query("SHOW server_version;");
+  const database_version_value = database_version_result.rows[0].server_version;
+
+  const database_max_connections_result = await database.query(
+    "SHOW max_connections;",
+  );
+  const database_max_connections_value = parseInt(
+    database_max_connections_result.rows[0].max_connections,
+  );
+
+  const database_name = process.env.POSTGRES_DB;
+  const database_opened_connections_result = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [database_name],
+  });
+  const database_opened_connections_value =
+    database_opened_connections_result.rows.length;
+
+  response.status(200).json({
+    updated_at: updated_at,
+    dependencies: {
+      database: {
+        version: database_version_value,
+        max_connections: database_max_connections_value,
+        opened_connections: database_opened_connections_value,
+        database_name: database_name,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const response_body = await response.json();
+
+  const parse_updated_at = new Date(response_body.updated_at).toISOString();
+  expect(response_body.updated_at).toEqual(parse_updated_at); //Se o responseboddy não é data ele não vai ser igual ao forçadamente toISOString
+
+  expect(response_body.dependencies.database.version).toEqual("16.0");
+  expect(response_body.dependencies.database.max_connections).toEqual(100);
+
+  expect(response_body.dependencies.database.opened_connections).toEqual(1);
+
+  console.log(response_body);
 });
