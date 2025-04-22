8b2aeb2b02e8c31b1736a67ecca3f3de11c7d8da
Rafael Martins <31152310+rafaelmartins3@users.noreply.github.com>
2025-04-22 05:33:55 +0000

    adding a certification to supbase connection

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,22 @@
 import { Client } from "pg";
+import fs from "fs";
 
 async function query(queryObject) {
+  const sslConfig =
+    process.env.NODE_ENV === "development"
+      ? false // Sem SSL no ambiente de desenvolvimento
+      : {
+          rejectUnauthorized: true, // Valida o certificado
+          ca: fs.readFileSync("./prod-ca-2021.crt").toString(), // Certificado fornecido
+        };
+
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: true,
+    ssl: sslConfig,
   });
 
   console.log("credenciais do postgres", {
